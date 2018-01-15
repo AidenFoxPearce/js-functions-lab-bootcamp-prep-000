3ad33cafdd23b6ddeca7fc23632b2b5fa632911f
BRAULIO R URBINA <braulio_urbina@hotmail.com>
2018-01-15 16:51:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
 
 console.log(happyHolidays())
 console.log(happyHolidaysTo("Abba"))
 console.log(happyHolidayTo("Hannukah", "Abba"))
-console.log(holidayCountdown(321, "Hannukah"))
+console.log(holidayCountdown("Hannukah", 321))
